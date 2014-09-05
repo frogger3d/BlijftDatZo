3d42c1075e3bbeb0c9d2c4c08cb950c71d7b5aef
Jordin <jordin@kee.nl>
2014-09-05 16:53:21 +0200

    Add particle object

---
+++ BlijftDatZo/Assets/BlijftDatZo/Scripts/Entities/Particle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.BlijftDatZo.Scripts;
+
+public class Particle : MonoBehaviour 
+{
+    private void Awake()
+    {
+
+    }
+    private void Start()
+    {
+
+    }
+}
