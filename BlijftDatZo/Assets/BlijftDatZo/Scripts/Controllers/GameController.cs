c3976ebd6dbb26b4f5a1a752a3063c6d04b3af22
Maarten Wesselius <mwesselius@gmail.com>
2014-09-06 08:51:01 +0200

    Added A and B sounds and added SoundPlayer.cs; GameController loads them from Resources folder

--- BlijftDatZo/Assets/BlijftDatZo/Scripts/Controllers/GameController.cs
+++ BlijftDatZo/Assets/BlijftDatZo/Scripts/Controllers/GameController.cs
@@ -6,6 +6,8 @@ using Assets.BlijftDatZo.Scripts;
 public class GameController : MonoBehaviour 
 {
     private GameObject prefabParticleStandard;
+	private AudioClip[] audioClipsA;
+	private AudioClip[] audioClipsB;
 	private List<Generator> allGenerators;
 
 	public List<ParticleBase> AllActiveParticles { 
@@ -16,6 +18,19 @@ public class GameController : MonoBehaviour
 	private void Awake()
     {
         this.prefabParticleStandard = (GameObject)Resources.Load(@"Prefabs/ParticleStandard");
+
+		this.audioClipsA = new AudioClip[6];
+		for (int i = 0; i < this.audioClipsA.Length; i++) 
+		{
+			this.audioClipsA [i] = (AudioClip)Resources.Load (@"Sounds/A" + (i + 1));
+		}
+
+		this.audioClipsB = new AudioClip[10];
+		for (int i = 0; i < this.audioClipsB.Length; i++) 
+		{
+			this.audioClipsB [i] = (AudioClip)Resources.Load (@"Sounds/B" + (i + 1));
+		}
+
 		this.AllActiveParticles = new List<ParticleBase> ();
     }
     public GameObject GetParticlePrefab()
@@ -23,13 +38,22 @@ public class GameController : MonoBehaviour
         return this.prefabParticleStandard;
     }
 
+	public AudioClip[] GetAudioClipsA()
+	{
+		return this.audioClipsA;
+	}
+	public AudioClip[] GetAudioClipsB()
+	{
+		return this.audioClipsB;
+	}
+
 	public void AddParticle(ParticleBase p)
 	{
-				this.AllActiveParticles.Add (p);
-		}
+		this.AllActiveParticles.Add (p);
+	}
 
 	public void RemoveParticle(ParticleBase p)
 	{
-				this.AllActiveParticles.Remove (p);
+		this.AllActiveParticles.Remove (p);
 	}
 }
